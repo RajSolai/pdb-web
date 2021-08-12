05b08ddbd7856184f1a936072df70f2325e145db
Solai Raj <msraj085@gmail.com>
2021-08-12 18:44:26 +0530

    added pathmap

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   reactStrictMode: true,
-}
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/project": { page: "/project" },
+    };
+  },
+};
